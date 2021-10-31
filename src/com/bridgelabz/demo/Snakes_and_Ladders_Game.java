b0e093014860f8b760ed5564a56e49e3a029dc77
priyankap <priyanka123360@gmail.com>
2021-10-31 21:53:08 +0530

    Added : Player intitial position

---
+++ src/com/bridgelabz/demo/Snakes_and_Ladders_Game.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.demo;
+
+public class Snakes_and_Ladders_Game {
+
+	public static void main(String[] args) {
+
+		System.out.print("Welcome to the Snakes and Ladders Game");
+
+		int player = 0;
+		System.out.print("Player intitial position: " + player);
+
+	}
+
+}
